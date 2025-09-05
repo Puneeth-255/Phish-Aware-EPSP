51a43f3e3684ba0537d92c278626efa1d14bb158
Puneeth-255 <puneethkumar740@gmail.com>
2025-09-05 15:48:31 +0530

    Delete upesps directory

--- upesps/public/Dashboard/training.js
+++
@@ -1,7 +0,0 @@
-const form = document.getElementById("assignForm");
-
-form.addEventListener("submit", function (e) {
-  e.preventDefault();
-  alert("✅ Training assigned (demo only)");
-  form.reset();
-});
