c611d8f3059f7357fafabade2f8cb54ca0c5b2cc
Puneeth-255 <puneethkumar740@gmail.com>
2025-09-05 16:08:46 +0530

    training.js

--- training.js
+++ training.js
@@ -2,6 +2,7 @@ const form = document.getElementById("assignForm");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  alert("✅ Training assigned (demo only)");
+  alert(" Training assigned (demo only)");
   form.reset();
 });
+
